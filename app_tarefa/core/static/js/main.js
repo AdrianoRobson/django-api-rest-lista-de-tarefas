312c8fa931fc71add518479a3150fbede42c8276
adriano <adriano08andrade@hotmail.com>
2021-11-25 13:35:32 -0300

    Criação do função para carregamento da lista de tarefas

--- app_tarefa/core/static/js/main.js
+++ app_tarefa/core/static/js/main.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
     
     SlickLoader.setText('Processando','Aguarde...'); 
     
-    //send()
+    send()
 
-    lst_tarefa = document.querySelectorAll('#home a')
+   /* lst_tarefa = document.querySelectorAll('#home a')
     
     for(let i = 0; i< lst_tarefa.length; i++){        
 
@@ -16,11 +16,14 @@ $(document).ready(function(){
 
         });    
 
-    } 
+    } */
 
     $(".btn").click(function () { 
 
-    }); 
+    });
+    
+    //$('#home').append('<a href="#" class="list-group-item list-group-item-action" id="'+1+'">'+2+'</a>')
+
     
     //$(".alert").alert() 
 });
@@ -36,13 +39,38 @@ function mostrarTarefa(){
     $("#home").hide()
 }
 
+function carregaListaTarefa(data){
+
+    $("#home a").remove(); 
+
+    for(i=0; i<data.length; i++){
+                
+        console.log('data: ',data[i].titulo)
+
+        $('#home').append('<a href="#" class="list-group-item list-group-item-action" id="'+data[i].id+'">'+data[i].titulo+'</a>')
+          
+    }   
+
+    lst_tarefa = document.querySelectorAll('#home a')
+
+    for(let i = 0; i< lst_tarefa.length; i++){        
+
+        $("#"+lst_tarefa[i].id).click(function () {  
+        
+            console.log(lst_tarefa[i].id)
+
+        });
+    
+    }
+}
+
 function send(tarefa_id='') { 
 
     SlickLoader.enable();
 
     $.ajax({
 
-        url: 'http://127.0.0.1:8000/lista/',
+        url: 'http://127.0.0.1:8000/lista/'+tarefa_id,
         type: 'GET',
         dataType: 'json',
         contentType: 'application/json',
@@ -56,20 +84,10 @@ function send(tarefa_id='') {
  
         },
         success: function (data) { 
+ 
+            carregaListaTarefa(data)
 
-            mostrarTarefa() 
-
-            for(i=0; i<data.length; i++){
-                
-                console.log('data: ',data[i].titulo)
-
-                $('#home').append('<a href="#" class="list-group-item list-group-item-action" id="'+data[i].id+'">'+data[i].titulo+'</a>')
-
-            }
-
-            SlickLoader.disable();   
-
-            mostrarTarefa() 
+            SlickLoader.disable();    
 
         }, 
     });
