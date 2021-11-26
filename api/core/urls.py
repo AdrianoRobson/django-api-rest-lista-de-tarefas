d7c09900af5a7a10552c65e18f737a7f9fc8b1bf
adriano <adriano08andrade@hotmail.com>
2021-11-26 13:00:29 -0300

    Conclução do CRUD da Tarefa na API

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,14 +1,19 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path 
 from rest_framework.authtoken.views import obtain_auth_token  
-from .views import tarefa_detail
+from .views import nota_lista, nota_cria, nota_detail
  
 urlpatterns = [
     
-    path('token-auth/', obtain_auth_token, name='api_token_auth'), 
-    
-    path('api/<str:pk>/', tarefa_detail),  
+    path('token-auth/', obtain_auth_token, name='api_token_auth'),  
 
-]
+    # retorna notas
+    path('api/notas/<str:pk>/', nota_lista),
 
-#urlpatterns = format_suffix_patterns(urlpatterns)
+    # cria nota
+    path('api/nota/',nota_cria), 
+
+    # retorna, atualiza, deleta nota
+    path('api/nota/<str:pk>/', nota_detail),
+ 
+
+] 
--- api/core/views.py
+++ api/core/views.py
@@ -6,8 +6,58 @@ from rest_framework import status
 from rest_framework.decorators import api_view 
 
 
+
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def tarefa_lista(request):
+
+    if request.method == 'GET':
+        #tarefas = Tarefa.objects.filter(lista_id=pk)   
+        #tarefas_serializer = TarefaSerializer(tarefas, many=True)
+        #return JsonResponse(tarefas_serializer.data, safe=False) 
+        pass 
+ 
+    elif request.method == 'POST':
+        tarefa_data = JSONParser().parse(request)
+        tarefa_serializer = TarefaSerializer(data=tarefa_data)
+        if tarefa_serializer.is_valid():
+            tarefa_serializer.save()
+            return JsonResponse(tarefa_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(tarefa_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        count = Tarefa.objects.all().delete()
+        return JsonResponse({'message': '{} Tutorials were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+
+
+# Cria uma nota
+@api_view(['POST'])
+def nota_cria(request):
+    nota_data = JSONParser().parse(request)
+    nota_serializer = TarefaSerializer(data=nota_data)
+
+    if nota_serializer.is_valid():
+        nota_serializer.save()
+        return JsonResponse(nota_serializer.data, status=status.HTTP_201_CREATED)
+
+    return JsonResponse(nota_serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+
+# Retorna lista de notas
+@api_view(['GET'])
+def nota_lista(request, pk):    
+  
+    if tarefas := Tarefa.objects.filter(lista_id=pk):   
+        tarefas_serializer = TarefaSerializer(tarefas, many=True)
+        return JsonResponse(tarefas_serializer.data, safe=False) 
+  
+    return JsonResponse({'message': 'The tarefa does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+  
+
+# Retorna, Atualiza, Deleta nota
 @api_view(['GET', 'PUT', 'DELETE'])
-def tarefa_detail(request, pk):
+def nota_detail(request, pk):
     try: 
         tarefa = Tarefa.objects.get(pk=pk) 
     except Tarefa.DoesNotExist: 
